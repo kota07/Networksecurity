036280cc8fd960f0629c461e063e8cbb63f9655c
Teja <kotateja09@gmail.com>
2026-05-10 21:13:24 +0530

    data_validation_completed

--- test_mongo.py
+++ test_mongo.py
@@ -3,7 +3,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://kotateja09_db_user:Admin123@cluster0.knyy7od.mongodb.net/?appName=Cluster0"
+
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
