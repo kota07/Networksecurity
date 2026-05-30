0b28c63cb843e38abee9184ed1ae820d08140731
Teja <kotateja09@gmail.com>
2026-05-30 22:41:54 +0530

    data_transformation_completed

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -18,4 +18,4 @@ class DataValidationArtifact:
 class DataTransformationArtifact:
     transformed_train_file_path: str
     transformed_test_file_path: str
-    preprocessed_object_file_path: str
+    transformed_object_file_path: str
